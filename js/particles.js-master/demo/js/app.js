2ae4fd6ff8e8d1bbbabe763cec48e8b3bca8a12b
Morris Salamon <Moishe@Salamons-MBP.home>
2017-05-21 01:09:42 -0400

    personal protfolio

---
+++ js/particles.js-master/demo/js/app.js
@@ -0,0 +1,8 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+particlesJS.load('particles-js', './js/particles.js-master/demo/particlesjs-config.json', function() {
+
+});
